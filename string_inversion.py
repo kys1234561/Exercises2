336c751a53dbc4a988c0edcb6db8620e22bd2b7e
kys <kys199803@1110.com>
2020-12-09 15:54:58 +0800

    add string_inversion.py

---
+++ string_inversion.py
@@ -0,0 +1,12 @@
+def str_reverse(str):
+    lists = list(str)
+    lists.reverse()#reverse是反向列表中元素
+    new_str = ''.join(lists)#用join函数来连接函数
+    print(new_str)
+
+
+def str_reverse2(str):
+    print(str[::-1])
+str = "string"
+str_reverse(str)
+str_reverse2(str)
